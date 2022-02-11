8be9b397702f1058206b296d5108920610b71a99
Antony <jns5@illinois.edu>
2022-02-11 04:36:53 +0100

    adding animations and striped line down the center.  going for street theme.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,13 @@ import "./styling/Home.css"
 function Home() {
   return (
     <div className="home">
+      <div class="dashed-line"> 
+        </div>
+        <div class="wrapper">
+  <div class="blue-sedan-move">
+    <img class = "blue-sedan" src= {require("./resources/car.png")}/>
+  </div>
+</div>
       <div class="data-container">
         <div class="row align-items-center my-5">
           <div class="col-lg-4">
@@ -19,26 +26,26 @@ function Home() {
               Hi, I'm Antony.  I'm studying Industiral Engineering and Computer Science @ UIUC.  Interested in web3, electric vehicles, and good design.
             </p>
             <p>
-             Currently working as a freelance developer on a variety of projects.  Currently studying abroad in Madrid and documenting my travels.
+             Currently working as a freelance developer on a variety of projects. 
             </p>
             <div className="row align-items-center">
-              <div className= "col-lg-1">
+              <div className= "col-lg-3">
               <img
                 class="social"
                 src= {require("./resources/twitter-sign.png")}
                 alt=""
               />
               </div>
-              <div className= "col-lg-1">
+              <div className= "col-lg-3">
               <img
                 class="social"
                 src= {require("./resources/github.png")}
                 alt=""
               />
               </div>
-              <div className= "col-lg-1">
+              <div className= "col-lg-3">
               <img
-                class="email"
+                class="social"
                 src= {require("./resources/email.png")}
                 alt=""
               />
@@ -47,6 +54,11 @@ function Home() {
           </div>
         </div>
       </div>
+      <div class="wrapper">
+  <div class="plane-move">
+    <img class = "plane" src= {require("./resources/plane.png")}/>
+  </div>
+</div>
     </div>
   );
 }
